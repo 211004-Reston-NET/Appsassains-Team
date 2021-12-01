96ee50f0cc2a2c965fba359dbd2d49c940e2e55a
Brian Stiner <Brian.Stiner@revature.net>
2021-12-01 12:33:21 -0500

    fixed white page

--- WebPaint2/src/app/canvas/canvas.component.ts
+++ WebPaint2/src/app/canvas/canvas.component.ts
@@ -1,58 +1,29 @@
-import { Component, ViewChild, ElementRef, OnInit, HostListener } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, HostListener, Inject, Injectable } from '@angular/core';
 
   @Component({
     selector: 'app-canvas',
-    template: '<canvas #myCanvas></canvas>',
+    templateUrl: './canvas.component.html',
+    styleUrls: ['./canvas.component.css']
   })
+  @Injectable()
   export class CanvasComponent implements OnInit {
 
-  @ViewChild('myCanvas', { static: false }) canvas: ElementRef<HTMLCanvasElement>;
-  ctx: CanvasRenderingContext2D;
+  canvas: HTMLCanvasElement | null;
+  ctx : CanvasRenderingContext2D | null;
 
-  constructor(private el: ElementRef<HTMLCanvasElement>, private img: HTMLImageElement) {
-    this.canvas = el;
-    this.ctx = this.canvas.nativeElement.getContext('2d')!;
-    this.img = img;
+  constructor(){
+    this.canvas = null;
+    this.ctx = null;
   }
-
-  nStartX = 0;
-  nStartY = 0;
-  bIsDrawing = false;
-  nDeltaX = 0;
-  nDeltaY = 0;
-  radius = 10;
 
   ngOnInit(): void {
+    this.canvas = document.getElementById('myCanvas')! as HTMLCanvasElement;
+    this.ctx = this.canvas.getContext('2d')! as CanvasRenderingContext2D;
+
+    console.log(this.canvas);
+    console.log(this.ctx);
   }
 
-  ClearCanvas() {
-    this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-  }
-
-  putPoint(e: MouseEvent){    // e is event: e.clientX and e.clientY are the coordinates of the mouse.
-    this.nStartX = e.clientX;
-    this.nStartY = e.clientY;
-    this.bIsDrawing = true;
-  }
-
-  stopPoint(){
-    this.bIsDrawing = false;
-  }
-
-  drawPoint(e: MouseEvent){        // This draws on mouse
-    if(!this.bIsDrawing){
-        return;
-    }
-    this.ctx.beginPath();
-    this.ctx.moveTo(this.nStartX, this.nStartY);
-    this.ctx.lineTo(e.clientX, e.clientY);
-    this.ctx.stroke();
-    this.nStartX = e.clientX;
-    this.nStartY = e.clientY;
-  }
-
-  @HostListener('window:mousedown', ['$event']) onclick(e: MouseEvent)  {this.putPoint(e);}
-  @HostListener('window:mousemove', ['$event']) onmove(e: MouseEvent)   {this.drawPoint(e);}
-  @HostListener('window:mouseup',   ['$event']) onup(e: MouseEvent)     {this.stopPoint();}
+  
 
 }
